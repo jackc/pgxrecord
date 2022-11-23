de57923b9e1577646372b21668321cdae4cbadff
Jack Christensen <jack@jackchristensen.com>
2022-11-22 21:13:52 -0500

    wip

--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module github.com/jackc/pgxrecord
 go 1.18
 
 require (
-	github.com/jackc/pgsql v0.0.0-20220720200728-bf8deec3ac70
 	github.com/jackc/pgx/v5 v5.1.1
+	github.com/stretchr/testify v1.8.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
 	golang.org/x/text v0.3.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pgxrecord.go
+++ pgxrecord.go
@@ -3,6 +3,7 @@ package pgxrecord
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -17,13 +18,14 @@ type Queryer interface {
 
 type Column struct {
 	Name       string
+	OID        uint32
+	NotNull    bool
 	PrimaryKey bool
 }
 
 type Table struct {
-	Name       pgx.Identifier
-	columns    []*Column
-	primaryKey []*Column
+	Name    pgx.Identifier
+	Columns []*Column
 }
 
 type Record struct {
@@ -33,14 +35,56 @@ type Record struct {
 }
 
 func (t *Table) LoadAllColumns(ctx context.Context, db Queryer) error {
-	db.Query(
-		ctx,
-		`select *
-from pg_catalog.pg_attribute
-where attrelid = $1::regclass
-	and attnum > 0
-	and not attisdropped
-order by attnum;`,
-		t.Name,
-	)
+	var tableOID uint32
+
+	{
+		var rows pgx.Rows
+
+		if len(t.Name) == 1 {
+			rows, _ = db.Query(ctx, `select c.oid
+	from pg_catalog.pg_class c
+	where c.relname=$1
+		and pg_catalog.pg_table_is_visible(c.oid)
+	limit 1`,
+				t.Name[0],
+			)
+		} else if len(t.Name) == 2 {
+			rows, _ = db.Query(ctx, `select c.oid
+	from pg_catalog.pg_class c
+		join pg_catalog.pg_namespace n on n.oid=c.relnamespace
+	where c.relname=$1
+		and n.nspname=$2
+		and pg_catalog.pg_table_is_visible(c.oid)
+	limit 1`,
+				t.Name[1], t.Name[0],
+			)
+		}
+
+		var err error
+		tableOID, err = pgx.CollectOneRow(rows, pgx.RowTo[uint32])
+		if err != nil {
+			return fmt.Errorf("failed to find table OID for %v: %v", t.Name, err)
+		}
+	}
+
+	rows, _ := db.Query(ctx, `select attname, atttypid, attnotnull,
+		coalesce((
+			select true
+			from pg_catalog.pg_index
+			where pg_index.indrelid=pg_attribute.attrelid
+				and pg_index.indisprimary
+				and pg_attribute.attnum = any(pg_index.indkey)
+		), false) as isprimary
+	from pg_catalog.pg_attribute
+	where attrelid=$1
+		and attnum > 0
+		and not attisdropped
+	order by attnum`, tableOID)
+	var err error
+	t.Columns, err = pgx.CollectRows(rows, pgx.RowToAddrOfStructByPos[Column])
+	if err != nil {
+		return fmt.Errorf("failed to find columns: %v", err)
+	}
+
+	return nil
 }
